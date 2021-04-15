d1d1b4cd3d755f0c526c58d8dd3af10c24ed3d8d
chuacw <1757930+chuacw@users.noreply.github.com>
2021-04-15 20:10:39 +0800

    Added Alloc method
    Added Alloc method to allocate memory for dynamic records.

--- Winapi.Defrag.Consts.pas
+++ Winapi.Defrag.Consts.pas
@@ -16,6 +16,18 @@ const
 // See https://social.technet.microsoft.com/wiki/contents/articles/24653.decoding-io-control-codes-ioctl-fsctl-and-deviceiocodes-with-table-of-known-values.aspx?Sort=MostUseful&PageIndex=1
   IOCTL_DISK_GET_CLUSTER_INFO = $00070214; // 0x00070214
 
+  MARK_HANDLE_PROTECT_CLUSTERS                    = $00000001;
+  MARK_HANDLE_TXF_SYSTEM_LOG                      = $00000004;
+  MARK_HANDLE_NOT_TXF_SYSTEM_LOG                  = $00000008;
+  MARK_HANDLE_REALTIME                            = $00000020;
+  MARK_HANDLE_NOT_REALTIME                        = $00000040;
+  MARK_HANDLE_READ_COPY                           = $00000080;
+  MARK_HANDLE_NOT_READ_COPY                       = $00000100;
+  MARK_HANDLE_RETURN_PURGE_FAILURE                = $00000400;
+  MARK_HANDLE_DISABLE_FILE_METADATA_OPTIMIZATION  = $00001000;
+  MARK_HANDLE_ENABLE_USN_SOURCE_ON_PAGING_IO      = $00002000;
+  MARK_HANDLE_SKIP_COHERENCY_SYNC_DISALLOW_WRITES = $00004000;
+
 implementation
 
 end.
--- Winapi.Defrag.Types.pas
+++ Winapi.Defrag.Types.pas
@@ -4,6 +4,8 @@ interface
 uses
   Winapi.Windows;
 
+{$RANGECHECKS OFF}
+
 type
   MOVE_FILE_DATA = record
     FileHandle: THandle;
@@ -12,25 +14,22 @@ type
     ClusterCount: Winapi.Windows.DWORD;
   end;
   PMOVE_FILE_DATA = ^MOVE_FILE_DATA;
-  TMoveFileData = MOVE_FILE_DATA;
-  PMoveFileData = ^TMoveFileData;
+  TMoveFileData   = MOVE_FILE_DATA;
+  PMoveFileData   = ^TMoveFileData;
 
   MARK_HANDLE_INFO = record
-  type
-    TDummyUnionName = record
-      MappedValue1: DWORD;
-      property UsnSourceInfo: DWORD read MappedValue1;
-      property CopyNumber: DWORD read MappedValue1;
+    DummyUnionName: record
+    case Byte of
+      0: (UsnSourceInfo: DWORD);
+      1: (CopyNumber: DWORD);
     end;
-  var
-    DummyUnionName: TDummyUnionName;
     UsnSourceInfo: DWORD;
     VolumeHandle: THandle;
     HandleInfo: DWORD;
   end;
   PMARK_HANDLE_INFO = ^MARK_HANDLE_INFO;
-  TMarkHandleInfo = MARK_HANDLE_INFO;
-  PMarkHandleInfo = ^TMarkHandleInfo;
+  TMarkHandleInfo   = MARK_HANDLE_INFO;
+  PMarkHandleInfo   = ^TMarkHandleInfo;
 
   TUSN = record end; // USN record
 
@@ -55,18 +54,20 @@ type
   TWords = TArray<Word>;
   TWord = Word;
   // output buffer for the FSCTL_GET_VOLUME_BITMAP control code.
+  PVOLUME_BITMAP_BUFFER = ^VOLUME_BITMAP_BUFFER;
   VOLUME_BITMAP_BUFFER = record
     StartingLcn: LARGE_INTEGER;
     BitmapSize: LARGE_INTEGER;
+    class function Alloc(BufferLen: DWORD): PVOLUME_BITMAP_BUFFER; static;
+    // The following is free flow, hence the Alloc method
     case Byte of
       0: (Buffer: array[0..0] of Byte); // array of Bytes, not just 1 byte!!!!
       1: (BufferUInt16: array[0..0] of UInt16);
       2: (BufferUInt32: array[0..0] of UInt32);
       3: (BufferUInt64: array[0..0] of UInt64);
   end;
-  PVOLUME_BITMAP_BUFFER = ^VOLUME_BITMAP_BUFFER;
-  TVolumeBitmapBuffer = VOLUME_BITMAP_BUFFER;
-  PVolumeBitmapBuffer = ^TVolumeBitmapBuffer;
+  TVolumeBitmapBuffer   = VOLUME_BITMAP_BUFFER;
+  PVolumeBitmapBuffer   = ^TVolumeBitmapBuffer;
 
   _GET_DISK_ATTRIBUTES = record
     Version: DWORD;
@@ -74,10 +75,10 @@ type
     Attributes: DWORDLONG;
     class operator Initialize(out Dest: _GET_DISK_ATTRIBUTES);
   end;
-  GET_DISK_ATTRIBUTES = _GET_DISK_ATTRIBUTES;
+  GET_DISK_ATTRIBUTES  = _GET_DISK_ATTRIBUTES;
   PGET_DISK_ATTRIBUTES = ^GET_DISK_ATTRIBUTES;
-  TGetDiskAttributes = GET_DISK_ATTRIBUTES;
-  PGetDiskAttributes = ^TGetDiskAttributes;
+  TGetDiskAttributes   = GET_DISK_ATTRIBUTES;
+  PGetDiskAttributes   = ^TGetDiskAttributes;
 
   DISK_CLUSTER_INFO = record
     Version: ULONG;
@@ -95,19 +96,20 @@ type
     StartingOffset: LARGE_INTEGER;
     ExtentLength: LARGE_INTEGER;
   end;
-  DISK_EXTENT = _DISK_EXTENT;
+  DISK_EXTENT  = _DISK_EXTENT;
   PDISK_EXTENT = ^DISK_EXTENT;
-  TDiskExtent = DISK_EXTENT;
-  PDiskExtent = ^TDiskExtent;
+  TDiskExtent  = DISK_EXTENT;
+  PDiskExtent  = ^TDiskExtent;
 
+  PVOLUME_DISK_EXTENTS = ^VOLUME_DISK_EXTENTS;
   _VOLUME_DISK_EXTENTS = record
     NumberOfDiskExtents: DWORD;
     Extents: array[0..0] of DISK_EXTENT;
+    class function Alloc(ExtentCount: DWORD): PVOLUME_DISK_EXTENTS; static;
   end;
-  VOLUME_DISK_EXTENTS = _VOLUME_DISK_EXTENTS;
-  PVOLUME_DISK_EXTENTS = ^VOLUME_DISK_EXTENTS;
-  TVolumeDiskExtents = VOLUME_DISK_EXTENTS;
-  PVolumeDiskExtents = ^TVolumeDiskExtents;
+  VOLUME_DISK_EXTENTS  = _VOLUME_DISK_EXTENTS;
+  TVolumeDiskExtents   = VOLUME_DISK_EXTENTS;
+  PVolumeDiskExtents   = ^TVolumeDiskExtents;
 
   NTFS_VOLUME_DATA_BUFFER = record
     VolumeSerialNumber: LARGE_INTEGER;
@@ -127,31 +129,37 @@ type
     class operator Initialize(out Dest: NTFS_VOLUME_DATA_BUFFER);
   end;
   PNTFS_VOLUME_DATA_BUFFER = ^NTFS_VOLUME_DATA_BUFFER;
-  TNTFSVolumeDataBuffer = NTFS_VOLUME_DATA_BUFFER;
-  PNTFSVolumeDataBuffer = ^TNTFSVolumeDataBuffer;
+  TNTFSVolumeDataBuffer    = NTFS_VOLUME_DATA_BUFFER;
+  PNTFSVolumeDataBuffer    = ^TNTFSVolumeDataBuffer;
 
   STARTING_LCN_INPUT_BUFFER = record
     StartingLcn: LARGE_INTEGER;
   end;
   PSTARTING_LCN_INPUT_BUFFER = ^STARTING_LCN_INPUT_BUFFER;
-  TStartingLcnInputBuffer = STARTING_LCN_INPUT_BUFFER;
-  PStartingLcnInputBuffer = ^TStartingLcnInputBuffer;
+  TStartingLcnInputBuffer    = STARTING_LCN_INPUT_BUFFER;
+  PStartingLcnInputBuffer    = ^TStartingLcnInputBuffer;
 
   STARTING_VCN_INPUT_BUFFER = record
     StartingVcn: LARGE_INTEGER;
   end;
   PSTARTING_VCN_INPUT_BUFFER = ^STARTING_VCN_INPUT_BUFFER;
-  TStartingVcnInputBuffer = STARTING_VCN_INPUT_BUFFER;
-  PStartingVcnInputBuffer = ^TStartingVcnInputBuffer;
+  TStartingVcnInputBuffer    = STARTING_VCN_INPUT_BUFFER;
+  PStartingVcnInputBuffer    = ^TStartingVcnInputBuffer;
 
-{$MESSAGE WARN 'Check definition!!! Incomplete!!!'}
-  RETRIEVAL_POINTERS_BUFFER = record
-    ExtentCount: DWORD;
-    StartingVcn: LARGE_INTEGER;
+  EXTENT = record
     NextVcn: LARGE_INTEGER;
     Lcn: LARGE_INTEGER;
   end;
+  PEXTENT = ^EXTENT;
+
+{$MESSAGE WARN 'Check definition!!! Incomplete!!!'}
   PRETRIEVAL_POINTERS_BUFFER = ^RETRIEVAL_POINTERS_BUFFER;
+  RETRIEVAL_POINTERS_BUFFER = record
+    ExtentCount: DWORD;
+    StartingVcn: LARGE_INTEGER;
+    Extents: array[0..0] of EXTENT; // This is free flow
+    class function Alloc(ExtentCount: DWORD): PRETRIEVAL_POINTERS_BUFFER; static;
+  end;
 
 implementation
 
@@ -177,6 +185,30 @@ class operator NTFS_VOLUME_DATA_BUFFER.Initialize(
   out Dest: NTFS_VOLUME_DATA_BUFFER);
 begin
   FillChar(Dest, SizeOf(Dest), 0);
+end;
+
+{ _VOLUME_DISK_EXTENTS }
+
+class function _VOLUME_DISK_EXTENTS.Alloc(ExtentCount: DWORD): PVOLUME_DISK_EXTENTS;
+begin
+  GetMem(Result, SizeOf(VOLUME_DISK_EXTENTS) +
+    (SizeOf(DISK_EXTENT) * (ExtentCount-1)));
+end;
+
+{ RETRIEVAL_POINTERS_BUFFER }
+
+class function RETRIEVAL_POINTERS_BUFFER.Alloc(ExtentCount: DWORD): PRETRIEVAL_POINTERS_BUFFER;
+begin
+  GetMem(Result, SizeOf(RETRIEVAL_POINTERS_BUFFER) + // Extents
+    (SizeOf(EXTENT) * (ExtentCount-1))); // remove the built-in array[0..0] of X
+  Result.ExtentCount := ExtentCount;
+end;
+
+{ VOLUME_BITMAP_BUFFER }
+
+class function VOLUME_BITMAP_BUFFER.Alloc(BufferLen: DWORD): PVOLUME_BITMAP_BUFFER;
+begin
+  GetMem(Result, SizeOf(VOLUME_BITMAP_BUFFER) + (BufferLen - SizeOf(Byte)));
 end;
 
 end.
